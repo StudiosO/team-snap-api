c7272b081119bac8535a98972b9ca0f8f937d91a
Milton Espinoza <mespinoza@rally-now.localdomain>
2018-01-18 21:14:18 +0000

    adding roles

---
+++ api/models/Roles.js
@@ -0,0 +1,21 @@
+/**
+ * Roles.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  connection: 'mongoServer',
+  attributes: {
+    name: {
+      type: 'string',
+      required: true      
+    },
+    users: {
+      collection: 'user',
+      via: 'role'
+    } 
+  }
+};
+
