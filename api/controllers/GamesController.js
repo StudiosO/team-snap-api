508dc1d11427eddd1a2f04f3168f9ece4dc3b4d0
Osmanyteam <jos.ojiron@gmail.com>
2018-01-24 12:05:17 -0600

    Metodo para guardar nuevos juegos

--- api/controllers/GamesController.js
+++ api/controllers/GamesController.js
@@ -6,6 +6,40 @@
  */
 
 module.exports = {
-	
+	newGame : (req, res)=>{
+        Games.create({
+            team : erq.team,
+            dateTime : req.body.dateTime,
+            attendenceTraking : req.body.attendenceTraking,
+            notifyTeam : req.body.notifyTeam,
+            optionalInfo : req.body.optionalInfo,
+        }, (error, game)=>{
+            if (error) { return res.serverError(err); }
+
+            LocationGame.create({
+                game : game.id,
+                address : req.body.address,
+                link : req.body.link,
+                detail : req.body.detail
+            }, (err, location)=>{
+                if (error) { return res.serverError(err); }
+
+                OppenentGame.create({
+                    game : game.id,
+                    name : req.body.name,
+                    person : req.body.person,
+                    phone : req.body.phone,
+                    email : req.body.email
+                }, (er, opponent)=>{
+
+                    res.json({
+                        game,
+                        location,
+                        opponent
+                    })
+                })
+            })
+        })
+    }
 };
 
